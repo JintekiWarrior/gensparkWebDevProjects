41b0eb373edcbeacfb26c9e3cdaa2db4da672f83
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-03 10:49:26 -0400

    Add contact page and form Add js to retrieve data from page

---
+++ basicWebPage/js/index.js
@@ -0,0 +1,32 @@
+const contactFormEl = document.querySelector("#contact-form");
+const errorMessage = document.querySelector("#error-message");
+
+const contactFormHandler = function (event) {
+  event.preventDefault();
+
+  const contactNameInput = document.querySelector(
+    "input[name='contact-name']"
+  ).value;
+  const contactEmailInput = document.querySelector(
+    "input[name='contact-email']"
+  ).value;
+  const contactPhoneInput = document.querySelector(
+    "input[name='contact-phone']"
+  ).value;
+  const contactMessageInput = document.querySelector(
+    "textarea[name='contact-message']"
+  ).value;
+
+  const contactDataObj = {
+    name: contactNameInput,
+    email: contactEmailInput,
+    phone: contactPhoneInput,
+    message: contactMessageInput,
+  };
+
+  try {
+  } catch (err) {}
+};
+
+// function to check if there is an error in creating the data
+contactFormEl.addEventListener("submit", contactFormHandler);

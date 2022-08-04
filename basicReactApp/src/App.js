b38b70a9f456fd4651abc9ff2d34648504deb75a
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-04 17:35:48 -0400

    Configure React router in root index and app files

--- basicReactApp/src/App.js
+++ basicReactApp/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
-import Store from "./components/Store/Store";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <NavBar />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 

6b74b0a67827e70f5eabaf25ed630a9bd18158af
JintekiWarrior <jintekiwarrior@gmail.com>
2022-08-04 17:59:48 -0400

    Configure routes to work in the NavBar Add SignUp component

--- basicReactApp/src/App.js
+++ basicReactApp/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "./components/Home/Home";
+import Store from "./components/Store/Store";
 import NavBar from "./components/NavBar/NavBar";
+import SignUp from "./components/SignUp/SignUp";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/Store" element={<Store />} />
+        <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </React.Fragment>
   );
--- basicReactApp/src/components/NavBar/NavBar.js
+++ basicReactApp/src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 import React from "react";
 
@@ -9,13 +10,21 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MMA Gear Here</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>MMA Gear Here</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#Store">Store</Nav.Link>
-            <Nav.Link href="#SignUp">SignUp</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/Store">
+              <Nav.Link>Store</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/SignUp">
+              <Nav.Link>SignUp</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
